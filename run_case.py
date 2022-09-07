b7199cfab392da124469d64a531e095c1a5791c8
yardasol <yardasol@grinnell.edu>
2022-09-07 17:01:05 -0500

    overhaul job submission

--- run_case.py
+++ run_case.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 from pathlib import Path
 from os import rename, remove
+import argparse
 #
 import numpy as np
 
@@ -11,35 +12,64 @@ from openmc.deplete.microxs import MicroXS
 from openmc.mpi import comm
 import openmc
 
-_case = 'case3'
-timedata = [(10 * [360], 's', 'minutes'),
-            (10 * [4], 'h', 'hours'),
-            (10 * [3], 'd', 'days'),
-            (10 * [30], 'd', 'months')]
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', choices=[1, 2, 3],
+                        type=int,
+                        default=1)
+    parser.add_argument('-i', choices=['predictor', 'cecm'],
+                        type=str,
+                        default='predictor')
+    parser.add_argument('-m', choices=['simple', 'full'],
+                        type=str,
+                        default='simple')
+    parser.add_argument('-t', choices=['minutes',' days', 'hours', 'months'],
+                        type=str,
+                        default='days')
+    parser.add_argument('-N',
+                        type=int,
+                        default=2,
+                        help='number of nodes to use in \
+                        distributed memory runs')
+    parser.add_argument('-n',
+                        type=int,
+                        default=4)
 
-integrators = [(PredictorIntegrator, 'predictor'),
-               (CECMIntegrator, 'cecm')]
+    args = parser.parse_args()
+    return int(args.c), str(args.i), str(args.m), str(args.t), int(args.N), int(args.n)
 
 
-#timedata = [(10 * [360], 's', 'minutes')]
-#timedata = [(10 * [4], 'h', 'hours')]
-timedata = [(10 * [3], 'd', 'days')]
-#timedata = [(10 * [30], 'd', 'months')]
+_case, integratorcase, depcase, timecase, N, n = parse_arguments()
 
+_case = f'case{_case}'
 
-depletion_cases = [('simple', '../openmc/tests/chain_simple.xml'),
-                   ('full', 'chain_endbf71_pwr.xml')]
-depletion_case = depletion_cases[0]
+timedata = {'minutes': (10 * [360], 's'),
+            'hours': (10 * [4], 'h'),
+            'days': (10 * [3], 'd'),
+            'months': (10 * [30], 'd')}
+
+integrators = {'predictor': PredictorIntegrator,
+               'cecm': CECMIntegrator}
+
+depletion_cases = {'simple': '../../openmc/tests/chain_simple.xml'),
+                   'full': '../chain_endbf71_pwr.xml'}
+
+timesteps, units = timedata[timecase]:
+Integrator = integrators[integratorcase]
+chain_file = depletion_cases[depcase]
 
 model = openmc.Model.from_xml()
 original_materials = deepcopy(model.materials)
-original_materials.export_to_xml()
 #operator_kwargs = {'normalization_mode': 'source-rate'}
 #integrator_kwargs = {'source_rates': 1164719970082145.0}
 
 operator_kwargs = {'normalization_mode': 'fission-q'}
 integrator_kwargs = {'power': 174} # W/cm
-depcase, chain_file = depletion_case
+
+runtime_dir = f'{_case}_{depcase}_{integratorcase}_{timecase}'
+if not os.path.exists(runtime_dir):
+    os.mkdir(runtime_dir)
+os.chdir(runtime_dir)
 
 if _case == 'case1':
     operator_kwargs['fission_yield_mode'] = 'constant'
@@ -49,60 +79,52 @@ if _case == 'case1':
     operator_args = (model,)
 elif _case == 'case2' or _case == 'case3':
     materials = original_materials
-    micro_xs = MicroXS.from_csv(f'micro_xs_{depcase}.csv')
+    micro_xs = MicroXS.from_csv(f'../micro_xs_{depcase}.csv')
     Operator = IndependentOperator
     operator_args = (materials, micro_xs, chain_file)
 
 cwd = Path(__file__).parents[0]
 
 if _case == 'case3':
-    for Integrator, integratorcase in integrators:
-        for timesteps, units, timecase in timedata:
-            materials = original_materials
-            micro_xs = MicroXS.from_csv(f'micro_xs_{depcase}.csv')
-            integrator_kwargs['timestep_units'] = units
-            # get i+1th value
-            timesteps += [timesteps[-1]]
-            for i, timestep in enumerate(timesteps):
-                operator = IndependentOperator(materials,
-                                               micro_xs,
-                                               chain_file,
-                                               **operator_kwargs)
+    materials = original_materials
+    micro_xs = MicroXS.from_csv(f'../micro_xs_{depcase}.csv')
+    integrator_kwargs['timestep_units'] = units
+    # get i+1th value
+    timesteps += [timesteps[-1]]
+    for i, timestep in enumerate(timesteps):
+        operator = IndependentOperator(materials,
+                                       micro_xs,
+                                       chain_file,
+                                       **operator_kwargs)
 
-                integrator = Integrator(operator,
-                                        [timestep],
-                                        **integrator_kwargs)
+        integrator = Integrator(operator,
+                                [timestep],
+                                **integrator_kwargs)
 
-                integrator.integrate()
-                results = Results(f'depletion_results.h5')
-                if comm.rank == 0:
-                    materials = results.export_to_materials(-1)
-                    rename(cwd / 'depletion_results.h5', cwd/ _case / integratorcase / f'{depcase}_depletion_results_{timecase}_{i}.h5' )
-                else:
-                    materials = None
-                materials = comm.bcast(materials, root=0)
-                comm.barrier()
+        integrator.integrate()
+        results = Results(f'depletion_results.h5')
+        materials = results.export_to_materials(-1)
+        rename(cwd / 'depletion_results.h5', cwd/ '..' / _case / integratorcase / f'{depcase}_depletion_results_{timecase}_{i}.h5' )
+        comm.barrier()
 
-                model.materials = materials
-                print("generating new microxs")
-                run_kwargs = {'mpi_args': ['srun', '-N2', '-n4', '--cpu-bind=socket']}
-                micro_xs = MicroXS.from_model(model,
-                                              model.materials[0],
-                                              chain_file,
-                                              run_kwargs=run_kwargs)
-                #micro_xs.to_csv(f'micro_xs_{depcase}_{i}.csv')
+        model.materials = materials
+        print("generating new microxs")
+        run_kwargs = {'mpi_args': ['srun', f'-N{N}', '-n{n}', '--cpu-bind=socket']}
+        micro_xs = MicroXS.from_model(model,
+                                      model.materials[0],
+                                      chain_file,
+                                      run_kwargs=run_kwargs)
+        #micro_xs.to_csv(f'micro_xs_{depcase}_{i}.csv')
 else:
-    for Integrator, integratorcase in integrators:
-        for timesteps, units, timecase in timedata:
-            operator = Operator(*operator_args, **operator_kwargs)
-            integrator_kwargs['timestep_units'] = units
-            integrator = Integrator(operator, timesteps, **integrator_kwargs)
-            integrator.integrate()
-            # move file based on metadata
-            if comm.rank == 0:
-                cwd = Path(__file__).parents[0]
-                rename(cwd / 'depletion_results.h5', cwd / _case / integratorcase / f'{depcase}_depletion_results_{timecase}.h5' )
-            comm.barrier()
-            #if _case == 'case1':
-            #    for i, t in enumerate(timesteps):
-            #        rename(cwd / f'openmc_simulation_n{i}.h5', cwd / _case / integrator_case / f'({depcase}_simulation_n{i}_{timecase}.h5')
+    operator = Operator(*operator_args, **operator_kwargs)
+    integrator_kwargs['timestep_units'] = units
+    integrator = Integrator(operator, timesteps, **integrator_kwargs)
+    integrator.integrate()
+    # move file based on metadata
+    if comm.rank == 0:
+        cwd = Path(__file__).parents[0]
+        rename(cwd / 'depletion_results.h5', cwd / '..' / _case / integratorcase / f'{depcase}_depletion_results_{timecase}.h5' )
+    comm.barrier()
+    #if _case == 'case1':
+    #    for i, t in enumerate(timesteps):
+    #        rename(cwd / f'openmc_simulation_n{i}.h5', cwd / _case / integrator_case / f'({depcase}_simulation_n{i}_{timecase}.h5')
