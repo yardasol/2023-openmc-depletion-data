ab2ec7e79ea9ff6551fb8b7144088dabfa3ea68c
yardasol <oyardas@anl.gov>
2022-09-07 17:20:19 -0500

    update generate-files script

--- sbatch_files/generate_files.sh
+++ sbatch_files/generate_files.sh
@@ -1,26 +1,35 @@
 NODES=2
 TIME="06:00:00"
-EMAIL="oyardas@anl.gov"
+EMAIL="oyardas2@illinois.edu"
 
 CASES=(1 2 3)
-TIMESCALES=('minutes' 'hours' 'days' 'months')
 INTEGRATORS=('predictor' 'cecm')
+MODES=('simple' 'full')
+TIMESCALES=('minutes' 'hours' 'days' 'months')
 for case in ${CASES[@]}
 do 
-  for scale in ${TIMESCALES[@]}
+  for integrator in ${INTEGRATORS[@]}
   do
-    for integrator in ${INTEGRATORS[@]}
-    do
-      JOBNAME="$case-$integrator-$scale"
-      FILENAME="case$JOBNAME.sbatch"
-      touch $FILENAME
-      echo -e "#!/bin/bash\n\n#SBATCH --nodes=$NODES\n#SBATCH --time=$TIME\n#SBATCH --partition=bdwall\n#SBATCH --account=openmcvalidation\n#SBATCH --job-name=$JOBNAME\n#SBATCH --mail-user=$EMAIL\n#SBATCH --mail-type=BEGIN,END,FAIL" > $FILENAME
-      echo -e "source $HOME/.bashrc\nmodule unload intel\nconda activate openmc-env\nexport OPENMC_CROSS_SECTIONS=\$(pwd)/../cross-section-libraries/endfb71_hdf5/cross_sections.xml\nNUM_RANKS=\$((SLURM_JOB_NUM_NODES * 2))" >> $FILENAME
+    for mode in ${MODES[@]}
+    do 
+      for scale in ${TIMESCALES[@]}
+      do
+        JOBNAME="$case-$scale-$integrator-$mode"
+        FILENAME="case$JOBNAME.sbatch"
+      	touch $FILENAME
+      	echo -e "#!/bin/bash\n\n#SBATCH --nodes=$NODES\n#SBATCH --time=$TIME\n#SBATCH --partition=bdwall\n#SBATCH --account=openmcvalidation\n#SBATCH --job-name=$JOBNAME\n#SBATCH --mail-user=$EMAIL\n#SBATCH --mail-type=BEGIN,END,FAIL" > $FILENAME
+      	echo -e "source \$HOME/.bashrc\nmodule unload intel\nconda activate openmc-env\nexport OPENMC_CROSS_SECTIONS=\$(pwd)/../cross-section-libraries/endfb71_hdf5/cross_sections.xml\nNUM_RANKS=\$((SLURM_JOB_NUM_NODES * 2))" >> $FILENAME
 
-      echo "srun -N \$SLURM_JOB_NUM_NODES \\" >> $FILENAME
-      echo "     -n \$NUM_RANKS\\" >> $FILENAME
-      echo "      --cpu-bind=socket \\" >> $FILENAME
-      echo "      python -u run_case$case.py -integrator $integrator -timescale $scale" >> $FILENAME
+        if [[ $case -eq 3 ]]
+        then
+          echo "srun -u python run_case.py -c$case -i $integrator -m $mode -t $scale -N \$SLURM_JOB_NUM_NODES -n \$NUM_RANKS" >> $FILENAME
+        else
+          echo "srun -N \$SLURM_JOB_NUM_NODES \\" >> $FILENAME
+          echo "     -n \$NUM_RANKS \\" >> $FILENAME
+          echo "      --cpu-bind=socket \\" >> $FILENAME
+          echo "      python -u run_case.py -c$case -i $integrator -m $mode -t $scale" >> $FILENAME
+        fi
+      done
     done
   done
 done
